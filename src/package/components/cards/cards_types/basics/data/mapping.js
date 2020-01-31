52c745b964c7f1152d8104be4cebc60fa4ff6f04
Clément Devos <cdevos.0@gmail.com>
2020-01-31 17:26:46 +0100

    mapping wrong again

--- src/package/components/cards/cards_types/basics/data/mapping.js
+++ src/package/components/cards/cards_types/basics/data/mapping.js
@@ -8,20 +8,22 @@ export const mapJsonResumeToBasicData = jsonResume => {
         studiesLevel: jsonResume?.specific?.education?.studiesLevel,
         codingYears: jsonResume?.specific?.work?.codingYears,
         codingReason: jsonResume?.specific?.work?.codingReason,
-        visaSponsorship: jsonResume?.basics?.visaSponsorship,
+        visaSponsorship: jsonResume?.specific?.basics?.visaSponsorship,
         searchState: jsonResume?.specific?.work?.searchState,
-        personalDescription: jsonResume?.specific?.basics.personalDescription
+        personalDescription: jsonResume?.specific?.basics?.personalDescription
     };
 };
 
 export const mapBasicsDataToJsonResume = data => ({
     basics: {
         summary: data.summary,
-        visaSponsorship: data.visaSponsorship,
-        personalDescription: data.personalDescription,
         location: data.currentCity && { ...data.currentCity, city: data.currentCity.name }
     },
     specific: {
+        basics: {
+            visaSponsorship: data.visaSponsorship,
+            personalDescription: data.personalDescription
+        },
         work: {
             experienceYears: data.experienceYears,
             codingYears: data.codingYears,
