86b2e3d1be11d63b8920427659c3875c6f7fe0cc
Clément Devos <cdevos.0@gmail.com>
2020-02-18 14:27:41 +0100

    fix(fetch) polyfill fails SSR https://github.com/github/fetch/issues/657

--- src/package/index.js
+++ src/package/index.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import { fetch as fetchPolyfill } from 'whatwg-fetch';
+import 'whatwg-fetch';
 import { DeveloperProfile } from './components/profile';
 
 import './styles/global.css';
