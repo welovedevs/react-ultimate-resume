bbdc862382bf1064be86f4174c836d914968ddf0
Thomas Grivet <thomas.grivet@tutanota.com>
2020-02-28 11:14:07 +0100

    From Profie to Profile

--- src/package/components/cards/cards_types/soundtrack/soundtrack_front/soundtrack_front.jsx
+++ src/package/components/cards/cards_types/soundtrack/soundtrack_front/soundtrack_front.jsx
@@ -5,7 +5,7 @@ import { createUseStyles } from 'react-jss';
 
 import { ProfileCardPaddedFront } from '../../../../commons/profile_card/profile_card_padded_front/profile_card_padding_front';
 import { CenterContentContainer } from '../../../../commons/center_content_container/center_content_container';
-import { ProfileCardFrontVector } from '../../../../commons/profile_card/profie_card_front_vector/profile_card_front_vector';
+import { ProfileCardFrontVector } from '../../../../commons/profile_card/profile_card_front_vector/profile_card_front_vector';
 import { ProfileCardFrontTypography } from '../../../../commons/profile_card/profile_card_front_typography/profile_card_front_typography';
 import { ProfileCardActions } from '../../../../commons/profile_card/profile_card_actions/profile_card_actions';
 import { ProfileCardButton } from '../../../../commons/profile_card/profile_card_button/profile_card_button';
