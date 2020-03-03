3215845e5346e78aaab89d5e4f5daef517c628b7
Thomas Grivet <thomas.grivet@tutanota.com>
2020-03-03 10:37:29 +0100

    New version build : 1.2.24-37.

--- components/profile.js
+++ components/profile.js
@@ -9,6 +9,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.DeveloperProfile = void 0;
 
+var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/defineProperty"));
+
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 
 var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
@@ -38,6 +40,10 @@ var _technologies_reducer = require("../store/technologies/technologies_reducer"
 var _contexts = require("../utils/context/contexts");
 
 var _footer = require("./footer/footer");
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { (0, _defineProperty2.default)(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 if (!Intl.PluralRules) {
   // eslint-disable-next-line global-require
@@ -159,7 +165,7 @@ var WithProvidersDeveloperProfile = function WithProvidersDeveloperProfile(_ref3
       classes = _ref3.classes,
       isEditing = _ref3.isEditing;
   var mergedOptions = (0, _react.useMemo)(function () {
-    return (0, _merge.default)(DEFAULT_OPTIONS, options);
+    return (0, _merge.default)(_objectSpread({}, DEFAULT_OPTIONS), options);
   }, [options]);
   console.log({
     mergedOptions: mergedOptions,
