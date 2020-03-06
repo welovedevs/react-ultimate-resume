4f08cff80adc3e96c33492512f0d6d1a64f92657
Clément Devos <cdevos.0@gmail.com>
2020-03-06 14:50:26 +0100

    fix(spotify) fixed null value in string utils

--- src/package/utils/string_utils.js
+++ src/package/utils/string_utils.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line no-bitwise
-export const hashCode = (str = '') => Array.from(str).reduce((s, c) => (Math.imul(31, s) + c.charCodeAt(0)) | 0, 0);
+export const hashCode = (str = '') => Array.from(str ?? '').reduce((s, c) => (Math.imul(31, s) + c.charCodeAt(0)) | 0, 0);
