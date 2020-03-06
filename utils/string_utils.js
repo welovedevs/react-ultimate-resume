5d901326fbff02d085d53e5640caa2c5e264c386
Clément Devos <cdevos.0@gmail.com>
2020-03-06 14:51:12 +0100

    New version build : 1.2.24-63.

--- utils/string_utils.js
+++ utils/string_utils.js
@@ -8,7 +8,7 @@ exports.hashCode = void 0;
 // eslint-disable-next-line no-bitwise
 var hashCode = function hashCode() {
   var str = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
-  return Array.from(str).reduce(function (s, c) {
+  return Array.from(str !== null && str !== void 0 ? str : '').reduce(function (s, c) {
     return Math.imul(31, s) + c.charCodeAt(0) | 0;
   }, 0);
 };
