20299bd79863b6756a1e1aaf8b03d40d533a820e
Thomas Grivet <thomas.grivet@tutanota.com>
2020-03-09 15:13:15 +0100

    Fixed onEdit merge function

--- src/package/components/cards/cards_types/gifs/gifs_card.jsx
+++ src/package/components/cards/cards_types/gifs/gifs_card.jsx
@@ -12,7 +12,9 @@ import { DeveloperProfileContext } from '../../../../utils/context/contexts';
 
 const GifsCardComponent = ({ variant, side }) => {
     const { data, isEditing, onEdit, mode } = useContext(DeveloperProfileContext);
+    console.log({ data });
     const mappedData = useMemo(() => mapInterestsFromJsonResume(data), [data]);
+
 
     const onDialogEdited = useCallback(editedData => {
         onEdit(mapInterestsToJsonResume(editedData));
