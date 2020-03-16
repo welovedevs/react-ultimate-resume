99d92090fa06dd1ad1cd854381e65f5ed4e74570
Thomas Grivet <thomas.grivet@tutanota.com>
2020-03-16 15:48:01 +0100

    Added additional node in Avatar

--- src/package/components/commons/avatar/avatar.jsx
+++ src/package/components/commons/avatar/avatar.jsx
@@ -3,14 +3,17 @@ import React from 'react';
 import { createUseStyles } from 'react-jss';
 
 import { styles } from './avatar_styles';
+import { useAdditionalNodes } from '../../hooks/use_additional_nodes';
 
 const useStyles = createUseStyles(styles);
 
 const AvatarComponent = ({ src = 'https://i.pravatar.cc/1000', displayedName }) => {
     const classes = useStyles();
+    const [nodes] = useAdditionalNodes('banner.avatar', null);
     return (
         <div className={classes.container}>
             <img className={classes.image} src={src} alt={displayedName} />
+            {nodes}
         </div>
     );
 };
