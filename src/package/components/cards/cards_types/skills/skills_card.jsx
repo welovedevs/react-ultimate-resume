bf4077a13b5f1d48a8b855c8788e88ea3545a45b
Clément Devos <cdevos.0@gmail.com>
2020-03-18 10:04:44 +0100

    flip this card

--- src/package/components/cards/cards_types/skills/skills_card.jsx
+++ src/package/components/cards/cards_types/skills/skills_card.jsx
@@ -37,7 +37,7 @@ const SkillsCardComponent = ({ variant }) => {
             }}
             data={mappedData}
             variant={variant}
-            side="back"
+            side={side}
         />
     );
 };
