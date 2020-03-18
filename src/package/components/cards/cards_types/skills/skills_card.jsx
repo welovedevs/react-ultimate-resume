478452fc1d684ce9617b83f52772dad0bdf5bff8
Clément Devos <cdevos.0@gmail.com>
2020-03-18 10:18:43 +0100

    flip this card

--- src/package/components/cards/cards_types/skills/skills_card.jsx
+++ src/package/components/cards/cards_types/skills/skills_card.jsx
@@ -9,7 +9,7 @@ import { mapSkillsFromJsonResume, mapSkillsToJsonResume } from './data/mapping';
 import { DeveloperProfileContext } from '../../../../utils/context/contexts';
 import { SkillsValidationSchema, validateSkillsComplete } from './data/validator';
 
-const SkillsCardComponent = ({ variant }) => {
+const SkillsCardComponent = ({ variant,side }) => {
     const { data, onEdit, isEditing, mode } = useContext(DeveloperProfileContext);
 
     const mappedData = useMemo(() => mapSkillsFromJsonResume(data), [data]);
