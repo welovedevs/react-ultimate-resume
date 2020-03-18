28196e529d6df377b1f35047edd07dce8530dedc
Thomas Grivet <thomas.grivet@tutanota.com>
2020-03-18 16:55:08 +0100

    Passing onFilesUpload function down the lemon tree

--- src/package/components/profile.jsx
+++ src/package/components/profile.jsx
@@ -114,6 +114,7 @@ const WithProvidersDeveloperProfile = ({
     additionalNodes,
     BeforeCards,
     classes,
+    onFilesUpload,
     isEditing,
     intl: parentIntl
 }) => {
@@ -141,6 +142,7 @@ const WithProvidersDeveloperProfile = ({
                     options={mergedOptions}
                     additionalNodes={additionalNodes}
                     BeforeCards={BeforeCards}
+                    onFilesUpload={onFilesUpload}
                     classes={classes}
                 />
             </IntlProvider>
