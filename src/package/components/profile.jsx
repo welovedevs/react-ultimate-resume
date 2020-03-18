c69fbfc38d9d6e78e79432466a4e1531b9652986
Clément Devos <cdevos.0@gmail.com>
2020-03-18 11:38:50 +0100

    switching back to dynamic locale

--- src/package/components/profile.jsx
+++ src/package/components/profile.jsx
@@ -49,20 +49,21 @@ const DEFAULT_OPTIONS = Object.freeze({
 });
 
 const DEFAULT_OBJECT = {};
-const DEFAULT_FUNCTION = () => {};
+const DEFAULT_FUNCTION = () => {
+};
 
 const DeveloperProfileComponent = ({
-    data = DEFAULT_OBJECT,
-    options,
-    mode,
-    onEdit: onEditProps = DEFAULT_FUNCTION,
-    onCustomizationChanged,
-    isEditing = false,
-    onFilesUpload = async () => 'https://source.unsplash.com/random/4000x2000',
-    BeforeCards,
-    additionalNodes,
-    classes: receivedGlobalClasses = {}
-}) => {
+                                       data = DEFAULT_OBJECT,
+                                       options,
+                                       mode,
+                                       onEdit: onEditProps = DEFAULT_FUNCTION,
+                                       onCustomizationChanged,
+                                       isEditing = false,
+                                       onFilesUpload = async () => 'https://source.unsplash.com/random/4000x2000',
+                                       BeforeCards,
+                                       additionalNodes,
+                                       classes: receivedGlobalClasses = {}
+                                   }) => {
     const { apiKeys, endpoints } = options;
     const classes = useStyles(styles);
 
@@ -96,26 +97,26 @@ const DeveloperProfileComponent = ({
     return (
         <div className={classes.container}>
             <DeveloperProfileContext.Provider value={context}>
-                <Banner customizationOptions={options.customization} onCustomizationChanged={onCustomizationChanged} />
+                <Banner customizationOptions={options.customization} onCustomizationChanged={onCustomizationChanged}/>
                 {BeforeCards}
-                <Cards cardsOrder={options.customization?.cardsOrder} side={side} />
-                {!options.dismissFooter && <Footer />}
+                <Cards cardsOrder={options.customization?.cardsOrder} side={side}/>
+                {!options.dismissFooter && <Footer/>}
             </DeveloperProfileContext.Provider>
         </div>
     );
 };
 
 const WithProvidersDeveloperProfile = ({
-    data,
-    onEdit,
-    onCustomizationChanged,
-    options = {},
-    mode = 'readOnly',
-    additionalNodes,
-    BeforeCards,
-    classes,
-    isEditing
-}) => {
+                                           data,
+                                           onEdit,
+                                           onCustomizationChanged,
+                                           options = {},
+                                           mode = 'readOnly',
+                                           additionalNodes,
+                                           BeforeCards,
+                                           classes,
+                                           isEditing
+                                       }) => {
     const mergedOptions = useMemo(
         () => mergeWith(cloneDeep(DEFAULT_OPTIONS), JSON.parse(JSON.stringify(options || {})), mergeOmitNull),
         [options]
@@ -129,7 +130,7 @@ const WithProvidersDeveloperProfile = ({
 
     return (
         <ThemeProvider theme={builtTheme}>
-            <IntlProvider locale={'en'} messages={messages.en} defaultLocale={locale}>
+            <IntlProvider locale={locale} messages={messages[locale] || messages.en} defaultLocale={locale}>
                 <DeveloperProfileComponent
                     isEditing={isEditing}
                     data={data}
