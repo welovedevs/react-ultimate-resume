36618ca366b4d0ff691e76bacfd9e9924037ef26
Thomas Grivet <thomas.grivet@tutanota.com>
2020-03-19 10:24:57 +0100

    New version build : 1.3.1-1.

--- components/cards/utils/cards_order.js
+++ components/cards/utils/cards_order.js
@@ -11,23 +11,23 @@ var DEFAULT_CARD_ORDER = [{
   type: 'projects',
   variant: 4
 }, {
-  type: 'language',
-  variant: 3
-}, {
   type: 'dreamjob',
   variant: 4
-}, {
-  type: 'gifs',
-  variant: 2
 }, {
   type: 'experiences',
   variant: 4
 }, {
+  type: 'skills',
+  variant: 0
+}, {
   type: 'studies',
   variant: 3
 }, {
-  type: 'skills',
-  variant: 0
+  type: 'language',
+  variant: 3
+}, {
+  type: 'gifs',
+  variant: 2
 }, {
   type: 'soundtrack',
   variant: 0
