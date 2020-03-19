1043fe6ff1b82ddca1d47d0999b82c0afbc0951f
Clément Devos <cdevos.0@gmail.com>
2020-03-19 18:10:37 +0100

    chore(eslint) c'était un peu agressif !

--- src/package/utils/string_utils.js
+++ src/package/utils/string_utils.js
@@ -1,3 +1,3 @@
-// eslint-disable-next-line no-bitwise
+/* eslint-disable no-bitwise */
 export const hashCode = (str = '') =>
     Array.from(str ?? '').reduce((s, c) => (Math.imul(31, s) + c.charCodeAt(0)) | 0, 0);
