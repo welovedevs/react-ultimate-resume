6ef7bcc4dc2a0e5b964e89db6dc9a81531864dc0
Thomas Grivet <thomas.grivet@tutanota.com>
2020-03-25 15:40:00 +0100

    Added height props in LoadingSpinner

--- src/package/components/commons/loading_spinner/loading_spinner.jsx
+++ src/package/components/commons/loading_spinner/loading_spinner.jsx
@@ -5,10 +5,10 @@ import { WaveLoading } from 'respinner';
 
 import { getHexFromPaletteColor } from '../../../utils/styles/styles_utils';
 
-const LoadingSpinnerComponent = ({ color = 'primary' }) => {
+const LoadingSpinnerComponent = ({ color = 'primary', height }) => {
     const theme = useTheme();
     const colorHex = useMemo(() => getHexFromPaletteColor(theme, color), [theme, color]);
-    return <WaveLoading stroke={colorHex} strokeWidth={3} count={2} />;
+    return <WaveLoading height={height} stroke={colorHex} strokeWidth={3} count={2} />;
 };
 
 export const LoadingSpinner = LoadingSpinnerComponent;
