e0a0bddd06c45f5870b5f71a6c9a63ddaa965273
Clément Devos <cdevos.0@gmail.com>
2020-03-25 22:34:04 +0100

    feat(banner) added jsonResume export

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React, { useCallback, useState } from 'react';
-import merge from 'lodash/merge';
+import { FormattedMessage } from 'react-intl';
+import { createUseStyles } from 'react-jss';
 
+import merge from 'lodash/merge';
 import isArray from 'lodash/isArray';
 import mergeWith from 'lodash/mergeWith';
 import cloneDeep from 'lodash/cloneDeep';
-import DeveloperProfile from './package';
-import JsonStub from './data/json_stub.json';
+import download from 'downloadjs';
+import { Button } from '@wld/ui';
 
+import JsonStub from './data/json_stub.json';
+import DeveloperProfile from './package';
+import { ReactComponent as SaveIcon } from './package/assets/icons/drop_file.svg';
+
+import { styles } from './app_styles';
+
+const useStyles = createUseStyles(styles);
 const mergeFunction = (objValue, srcValue) => {
     if (!objValue || isArray(objValue)) {
         return srcValue;
@@ -14,7 +23,9 @@ const mergeFunction = (objValue, srcValue) => {
     return merge(objValue, srcValue);
 };
 
+const mode = 'edit';
 function App() {
+    const classes = useStyles();
     const [data, setData] = useState(JsonStub);
 
     const onEdit = useCallback(newData => setData(mergeWith(cloneDeep(data), newData, mergeFunction)), [
@@ -24,22 +35,36 @@ function App() {
 
     const onCustomizationChanged = useCallback(setCustomization, [data]);
 
+    const handleClick = useCallback(async () => {
+        download(JSON.stringify(data), `${`Resume-${data?.basics?.name || 'Developer'}`.replace(' ', '-')}.json`);
+    }, [JSON.stringify(data)]);
+
     return (
         <DeveloperProfile
-            mode="edit"
+            mode={mode}
             data={data}
             onEdit={onEdit}
             onCustomizationChanged={onCustomizationChanged}
             options={{
                 // side: 'back',
                 apiKeys: {
-                    giphy: 'LlyqILDRkoJ0vTRQknbiM5QhmHCK4GmM'
+                    giphy: process.env.REACT_APP_GIPHY
                 },
                 endpoints: {
                     devicons:
                         'https://firebasestorage.googleapis.com/v0/b/jechercheundev.appspot.com/o/technologies%2Ftechnologies_list.json?alt=media&token=459028ba-d9bc-4480-a3c4-88633afab7e2'
                 },
                 customization
+            }}
+            additionalNodes={{
+                banner: {
+                    actionsButtons: mode === 'edit' && (
+                        <Button variant="outlined" onClick={handleClick} color={'light'}>
+                            <SaveIcon className={classes.saveIcon} />
+                            <FormattedMessage id="Profile.header.jsonResume.download" defaultMessage="Export" />
+                        </Button>
+                    )
+                }
             }}
         />
     );
---
+++ src/app_styles.js
@@ -0,0 +1,9 @@
+export const styles = {
+    saveIcon: {
+        marginRight: 8,
+        width: 16,
+        '& g': {
+            stroke: 'white'
+        }
+    }
+};
