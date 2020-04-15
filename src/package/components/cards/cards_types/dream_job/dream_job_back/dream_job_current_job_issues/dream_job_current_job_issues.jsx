d79146655b5523a096f19a9897b24815b83ab6e1
Clément Devos <clement@welovedevs.com>
2020-04-15 16:49:26 +0200

    fix(dreamjob) fixed checkbox groups not working properly, also refactoring and debouncing others field  #32

--- src/package/components/cards/cards_types/dream_job/dream_job_back/dream_job_current_job_issues/dream_job_current_job_issues.jsx
+++ src/package/components/cards/cards_types/dream_job/dream_job_back/dream_job_current_job_issues/dream_job_current_job_issues.jsx
@@ -5,6 +5,7 @@ import { useIntl } from 'react-intl';
 
 import { styles } from './dream_job_current_job_issues_styles';
 import { translations } from '../../../../../../utils/enums/job_issues/job_issues_translations';
+import { JobIssues } from '../../../../../../utils/enums/job_issues/job_issues_utils';
 
 const useStyles = createUseStyles(styles);
 
@@ -15,9 +16,16 @@ const DreamJobCurrentJobIssuesComponent = ({ currentJobIssues = {} }) => {
         <ul className={classes.list}>
             {Object.entries(currentJobIssues)
                 .filter(([, value]) => Boolean(value))
-                .map(([issueId]) => {
+                .map(([issueId, value]) => {
                     if (!translations[issueId]) {
                         return null;
+                    }
+                    if (issueId === JobIssues.OTHER) {
+                        return (
+                            <li className={classes.listItem} key={`dream_job_current_job_issue_${issueId}`}>
+                                {value}
+                            </li>
+                        );
                     }
                     return (
                         <li className={classes.listItem} key={`dream_job_current_job_issue_${issueId}`}>
