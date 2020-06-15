e3911306678bd732c5846d4d46264954514e831c
Clément Devos <cdevos.0@gmail.com>
2020-06-15 16:50:13 +0200

    fix(projects) fixed project not sorted even using indexes

--- src/package/components/cards/cards_types/projects/data/mapping.js
+++ src/package/components/cards/cards_types/projects/data/mapping.js
@@ -2,13 +2,15 @@ import uuid from 'uuid/v4';
 import moment from 'moment';
 
 export const mapProjectsFromJsonResume = (jsonResume) => ({
-    projects: jsonResume?.projects?.map((project, index) => ({
-        ...project,
-        // generating uuid for manipulating data if not present
-        id: project.id || uuid(),
-        date: project.endDate && moment(project.endDate, 'YYYY-MM-DD'),
-        index: project.index || index
-    }))
+    projects: jsonResume?.projects
+        ?.map((project, index) => ({
+            ...project,
+            // generating uuid for manipulating data if not present
+            id: project.id || uuid(),
+            date: project.endDate && moment(project.endDate, 'YYYY-MM-DD'),
+            index: project.index || index
+        }))
+        .sort(({ index: a }, { index: b }) => a - b)
 });
 
 export const mapProjectToJsonResume = (project) => ({
