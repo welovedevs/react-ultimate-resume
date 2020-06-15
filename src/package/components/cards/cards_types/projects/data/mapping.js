7c531edd8eed897f040339bf9d6a9231bb4e5ce5
Clément Devos <cdevos.0@gmail.com>
2020-06-15 17:04:31 +0200

    Javascript  bliss in action here

--- src/package/components/cards/cards_types/projects/data/mapping.js
+++ src/package/components/cards/cards_types/projects/data/mapping.js
@@ -3,13 +3,16 @@ import moment from 'moment';
 
 export const mapProjectsFromJsonResume = (jsonResume) => ({
     projects: jsonResume?.projects
-        ?.map((project, index) => ({
-            ...project,
-            // generating uuid for manipulating data if not present
-            id: project.id || uuid(),
-            date: project.endDate && moment(project.endDate, 'YYYY-MM-DD'),
-            index: project.index || index
-        }))
+        ?.map((project, index) => {
+            const projectIndex = Number(project.index);
+            return {
+                ...project,
+                // generating uuid for manipulating data if not present
+                id: project.id || uuid(),
+                date: project.endDate && moment(project.endDate, 'YYYY-MM-DD'),
+                index: Number.isNaN(projectIndex) ? index : projectIndex
+            };
+        })
         .sort(({ index: a }, { index: b }) => a - b)
 });
 
