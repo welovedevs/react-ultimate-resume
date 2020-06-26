b0e7e852838319a4952dab8b1211e84fe3ec9879
Vincent Cotro <vincentcotro@googlemail.com>
2020-06-26 13:26:56 +0200

    Update skills card is incomplete validation schema

--- src/package/components/cards/cards_types/skills/data/validator.js
+++ src/package/components/cards/cards_types/skills/data/validator.js
@@ -2,6 +2,12 @@ import * as Yup from 'yup';
 import { skillTranslations } from './validator_translations';
 
 const DEFAULT_MAX_ITEMS = 12;
+
+export const SkillsIsCompleteValidationSchema = (formatMessage = () => {}) =>
+    Yup.object({
+        skills: Yup.array().required(formatMessage(skillTranslations.atLeastOne)).min(1)
+    });
+
 export const SkillsValidationSchema = (formatMessage = () => {}, options = { limitTo: DEFAULT_MAX_ITEMS }) => {
     const count = Number.isNaN(Number(options.limitTo)) ? DEFAULT_MAX_ITEMS : options.limitTo;
     return Yup.object({
@@ -13,7 +19,7 @@ export const SkillsValidationSchema = (formatMessage = () => {}, options = { lim
 };
 export const validateSkillsComplete = (data) => {
     try {
-        SkillsValidationSchema().validateSync(data);
+        SkillsIsCompleteValidationSchema().validateSync(data);
     } catch (e) {
         return false;
     }
