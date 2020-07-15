ca17fe291d1bad6a655a3f0e17890c92db51cc9c
Thomas Grivet <thomas.grivet@tutanota.com>
2020-07-15 16:14:28 +0200

    Added width: 100% on cards' container

--- src/package/components/cards/cards_styles.js
+++ src/package/components/cards/cards_styles.js
@@ -16,6 +16,7 @@ export const styles = (theme) => {
             zIndex: 3
         },
         cards: ({ maxCardsPerRow }) => ({
+            width: '100%',
             display: 'flex',
             justifyContent: 'center',
             flexWrap: 'wrap',
