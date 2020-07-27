bcf5ac27482dc7067f8d251f3b0fa5a25f57a730
Clément Devos <cdevos.0@gmail.com>
2020-07-27 14:40:57 +0200

    feat(styling) slowly going back to mui styles themes providers

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { StylesProvider } from '@material-ui/core/styles';
+import { DEFAULT_THEME } from '@welovedevs/ui/styles/theme';
+import { StylesProvider as MuiStylesProvider, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import './styles/global.css';
 import './styles/animations.css';
+import { create } from 'jss';
+import { JssProvider, ThemeProvider as ReactJSSThemeProvider } from 'react-jss';
+import jssDefaultPreset from 'jss-preset-default';
+
+const muiInstance = create(jssDefaultPreset());
+muiInstance.setup({ insertionPoint: 'mui-insertion-point' });
+const jssinstance = create(jssDefaultPreset());
+jssinstance.setup({ insertionPoint: 'jss-insertion-point' });
+
+export const theme = createMuiTheme({
+    ...DEFAULT_THEME,
+    spacing: 8,
+    palette: Object.entries(DEFAULT_THEME.palette).reduce((acc, [name, values]) => {
+        const accCopy = acc;
+        accCopy[name].main = values[500];
+        return accCopy;
+    }, DEFAULT_THEME.palette)
+});
 
 ReactDOM.render(
-    <StylesProvider injectFirst>
-        <App />
-    </StylesProvider>,
+    <MuiStylesProvider jss={muiInstance}>
+        <JssProvider jss={jssinstance}>
+            <ThemeProvider {...{ theme }}>
+                <App />
+            </ThemeProvider>
+        </JssProvider>
+    </MuiStylesProvider>,
     document.getElementById('root')
 );
