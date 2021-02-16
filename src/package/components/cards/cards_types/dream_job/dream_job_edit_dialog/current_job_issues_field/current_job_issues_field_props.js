e38d36fd3bd7643653ec242764864fc4a09b8b23
val <valentin@welovedevs.com>
2021-02-16 18:09:13 +0100

    currentJob issue field animation done

--- src/package/components/cards/cards_types/dream_job/dream_job_edit_dialog/current_job_issues_field/current_job_issues_field_props.js
+++ src/package/components/cards/cards_types/dream_job/dream_job_edit_dialog/current_job_issues_field/current_job_issues_field_props.js
@@ -1,7 +1,8 @@
-export const CURRENT_JOB_ISSUES_FIELD_OTHER_TEXTFIELD_TRANSITIONS_SPRING_PROPS = {
-    from: {
+export const CURRENT_JOB_ISSUES_FIELD_OTHER_TEXTFIELD_TRANSITIONS_PROPS = {
+    initial: {
         opacity: 0,
-        transform: 'translate3d(-20px, 0, 0)'
+        transform: 'translate3d(-20px, 0, 0)',
+        transitionDuration: '0.3s'
     },
     enter: {
         opacity: 1,
