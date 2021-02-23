71494fcea7b58e016597fe60ca3df8edbb8a04f4
Vincent Cotro <vincentcotro@googlemail.com>
2021-02-23 15:09:38 +0400

    Update profile card button

--- src/package/components/commons/profile_card/profile_card_button/profile_card_button.jsx
+++ src/package/components/commons/profile_card/profile_card_button/profile_card_button.jsx
@@ -11,40 +11,22 @@ import { styles } from './profile_card_button_styles';
 import { useCardVariant } from '../../../hooks/profile_card_hooks/use_card_variant';
 import { DEFAULT_SPRING_TYPE as spring } from '../../../../utils/framer_motion/common_types/spring_type';
 
-const DEFAULT_MOTION_PROPS = 0;
-const ACTIVE_MOTION_PROPS = 6;
-
-const ProfileCardButtonComponent = injectSheet(styles)(({ overrideColor, classes, children, ...other }) => {
-    const [motionProps, setMotionProps] = useState(() => DEFAULT_MOTION_PROPS);
-    const setDefaultMotionProps = useCallback(() => setMotionProps(() => DEFAULT_MOTION_PROPS), []);
-    const setActiveMotionProps = useCallback(() => setMotionProps(() => ACTIVE_MOTION_PROPS), []);
-
-    return (
+const ProfileCardButtonComponent = injectSheet(styles)(({ overrideColor, classes, children, ...other }) =>
+    (
         <div className={classes.container}>
             <Button
                 classes={{ container: classes.button, typography: classes.typography }}
                 size="small"
                 variant="text"
-                onMouseEnter={setActiveMotionProps}
-                onMouseLeave={setDefaultMotionProps}
-                onFocus={setActiveMotionProps}
-                onBlur={setDefaultMotionProps}
                 {...other}
             >
                 {children}
             </Button>
-            <motion.span
-                className={classes.arrowContainer}
-                animate={{
-                    x: `${motionProps}px`
-                }}
-                transition={spring}
-            >
-                <ArrowRight className={classes.arrow} />
+            <motion.span className={classes.arrowContainer} whileHover={{ x: 6 }} transition={spring}>
+                <ArrowRight className={classes.arrow}/>
             </motion.span>
         </div>
-    );
-});
+    ));
 
 const InjectVariantProfileCardButton = (props) => {
     const [variant] = useCardVariant();
