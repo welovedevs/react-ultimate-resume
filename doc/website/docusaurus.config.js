cc7f059c0b5cc310d20bebc0a14665471b087fa7
Clément Devos <clement@welovedevs.com>
2021-12-17 16:36:27 +0100

    docusaurus update?

--- doc/website/docusaurus.config.js
+++ doc/website/docusaurus.config.js
@@ -1,7 +1,11 @@
-console.log('Env is ', process.env.NODE_ENV);
-require('dotenv').config({ path: `.env.${process.env.NODE_ENV || 'local'}` });
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
 
-module.exports = {
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
     title: 'React Ultimate Resume',
     tagline: 'Our vision for a clean looking and awesome react-powered dev resume.',
     url: 'https://welovedevs.com/app',
@@ -9,69 +13,85 @@ module.exports = {
     favicon: 'img/favicon.ico',
     organizationName: 'welovedevs', // Usually your GitHub org/user name.
     projectName: 'react-ultimate-resume', // Usually your repo name.
-    themeConfig: {
-        navbar: {
-            title: 'Docs',
-            logo: {
-                alt: 'WeLoveDevs Logo',
-                src: 'https://welovedevs.com/wp-content/uploads/2019/12/logo-menu-welovedevs-220bad.svg'
-            },
-            items: [
-                {
-                    href: 'https://welovedevs.com/inspiration',
-                    label: 'Blog',
-                    position: 'right'
-                }
-            ]
-        },
-        footer: {
-            style: 'dark',
-            links: [
-                {
-                    title: 'Community',
-                    items: [
-                        {
-                            label: 'Discord',
-                            href: 'https://discord.gg/udbbbAq'
-                        }
-                    ]
-                },
-                {
-                    title: 'Social',
-                    items: [
-                        {
-                            label: 'Blog',
-                            href: 'https://welovedevs.com/inspiration'
-                        },
-                        {
-                            label: 'GitHub',
-                            href: 'https://github.com/facebook/welovedevs'
-                        },
-                        {
-                            label: 'Twitter',
-                            href: 'https://twitter.com/welovedevs'
-                        }
-                    ]
-                }
-            ],
-            copyright: `Copyright © ${new Date().getFullYear()} WeLoveDevs.com. Built with Docusaurus.`
-        },
-        googleAnalytics: {
-            trackingID: 'UA-71913303-3'
-        }
-    },
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
     presets: [
         [
-            '@docusaurus/preset-classic',
-            {
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
-                    editUrl: 'https://github.com/welovedevs/docs/website/'
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/'
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/blog/'
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css')
                 }
-            }
+            })
         ]
-    ]
+    ],
+
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: 'Docs',
+                logo: {
+                    alt: 'WeLoveDevs Logo',
+                    src: 'https://welovedevs.com/wp-content/uploads/2019/12/logo-menu-welovedevs-220bad.svg'
+                },
+                items: [
+                    {
+                        href: 'https://welovedevs.com/inspiration',
+                        label: 'Blog',
+                        position: 'right'
+                    }
+                ]
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Community',
+                        items: [
+                            {
+                                label: 'Discord',
+                                href: 'https://discord.gg/udbbbAq'
+                            }
+                        ]
+                    },
+                    {
+                        title: 'Social',
+                        items: [
+                            {
+                                label: 'Blog',
+                                href: 'https://welovedevs.com/inspiration'
+                            },
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/facebook/welovedevs'
+                            },
+                            {
+                                label: 'Twitter',
+                                href: 'https://twitter.com/welovedevs'
+                            }
+                        ]
+                    }
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} WeLoveDevs.com. Built with Docusaurus.`
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme
+            }
+        })
 };
+
+module.exports = config;
