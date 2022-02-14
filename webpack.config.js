db9fe657ce31627067f86d1b36a70c99dd97ceb1
Vincent Cotro <vincentcotro@googlemail.com>
2022-02-14 16:32:34 +0400

    Adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const glob = require('glob');
+
+module.exports = {
+    entry: {
+        'bundle.js': glob.sync('./build/static/?(js|css|media)/main.*.?(js|css)').map((f) => path.resolve(__dirname, f))
+    },
+    output: {
+        filename: 'build/static/js/bundle.min.js'
+    },
+    resolve: {
+        roots: [__dirname, path.resolve(__dirname, 'build')],
+    },
+    module: {
+        rules: [
+            { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    }
+};
