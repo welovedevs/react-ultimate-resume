16f1a50ee105921364b4be151a4793a66e20ac08
clementdevos <cdevos.0@gmail.com>
2023-03-29 11:55:13 +0200

    fix build

--- doc/website/src/pages/index.js
+++ doc/website/src/pages/index.js
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
@@ -15,7 +14,7 @@ function HomepageHeader() {
                 <h1 className="hero__title">{siteConfig.title}</h1>
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
                 <div className={styles.buttons}>
-                    <Link className="button button--secondary button--lg" to="/docs/intro">
+                    <Link className="button button--secondary button--lg" to="/docs/home">
                         Docusaurus Tutorial - 5min ⏱️
                     </Link>
                 </div>
@@ -29,9 +28,6 @@ export default function Home() {
     return (
         <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
             <HomepageHeader />
-            <main>
-                <HomepageFeatures />
-            </main>
         </Layout>
     );
 }
