4e0442fade3fdb1a70eabbeba1d8f77c1a251449
clementdevos <cdevos.0@gmail.com>
2023-03-29 15:54:32 +0200

    baseurl

--- doc/website/docusaurus.config.js
+++ doc/website/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
     url: 'https://your-docusaurus-test-site.com',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+    baseUrl: '/react-ultimate-resume',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
