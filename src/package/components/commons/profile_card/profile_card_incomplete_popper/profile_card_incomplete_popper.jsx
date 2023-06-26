0ed43ab5c97b6334d7cc2690ae33d9c7f1b8f569
clementdevos <cdevos.0@gmail.com>
2023-06-26 22:22:21 +0200

    fix(popper) fixed popper colors not rendered properly, simplified styling

--- src/package/components/commons/profile_card/profile_card_incomplete_popper/profile_card_incomplete_popper.jsx
+++ src/package/components/commons/profile_card/profile_card_incomplete_popper/profile_card_incomplete_popper.jsx
@@ -1,19 +1,13 @@
-import React, { memo, useEffect, useState } from 'react';
+import React, {memo, useEffect, useState} from 'react';
+import {FormattedMessage} from 'react-intl';
 
-import makeStyles from '@mui/styles/makeStyles';
-import { FormattedMessage } from 'react-intl';
+import {PopperCard, Typography} from '@welovedevs/ui';
 
-import { PopperCard, Typography } from '@welovedevs/ui';
+import {ReactComponent as WarnIcon} from '../../../../assets/icons/warn.svg';
+import {useMode} from '../../../hooks/use_mode';
 
-import { ReactComponent as WarnIcon } from '../../../../assets/icons/warn.svg';
 
-import { styles } from './profile_card_incomplete_popper_styles';
-import { useMode } from '../../../hooks/use_mode';
-
-const useStyles = makeStyles(styles);
-
-const ProfileCardIncompletePopperComponent = ({ open, onClose, anchorElement }) => {
-    const classes = useStyles();
+const ProfileCardIncompletePopperComponent = ({open, onClose, anchorElement}) => {
     const [hasBeenMounted, setHasBeenMouneted] = useState(false);
     const [mode] = useMode();
 
@@ -26,8 +20,9 @@ const ProfileCardIncompletePopperComponent = ({ open, onClose, anchorElement }) 
     return (
         <PopperCard
             classes={{
-                container: classes.container,
-                arrowContainer: classes.arrowContainer
+
+                container: "bg-danger-400",
+                arrowContainer: "text-danger-400"
             }}
             open={open}
             onClose={onClose}
@@ -35,21 +30,11 @@ const ProfileCardIncompletePopperComponent = ({ open, onClose, anchorElement }) 
             popperProps={{
                 placement: 'top',
                 disablePortal: true,
-                modifiers: [
-                    {
-                        name: 'preventOverflow'
-                    },
-                    {
-                        name: 'hide'
-                    },
-                    {
-                        name: 'flip'
-                    }
-                ]
             }}
         >
-            <WarnIcon className={classes.icon} />
-            <Typography color="light">
+
+            <Typography color="light" className="flex items-center">
+                <WarnIcon className="mr-1 w-3 h-3 fill-current"/>
                 <FormattedMessage
                     id="ProfileCardIncompletePopper.label.value"
                     defaultMessage="This card is missing data"
